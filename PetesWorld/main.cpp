d27be0df5cf09bc1d3c16ffdf3230c4fc1178c52
(Student  C00302210) Robert Mc Gregor <C00302210@setu.ie>
2023-11-10 10:58:12 +0000

    Last commit before merge

--- PetesWorld/main.cpp
+++ PetesWorld/main.cpp
@@ -23,7 +23,7 @@ int main()
 		return 0;
 	}
 
-	std::cout << "Pick a letter from A to C or V to Z" << std::endl; // change this line each iteration
+	std::cout << "Pick a letter from A to C or U to Z" << std::endl; // change this line each iteration
 	std::cin >> letter;
 
 	if (letter < 'A' || letter > 'Z') // not a capital letter
